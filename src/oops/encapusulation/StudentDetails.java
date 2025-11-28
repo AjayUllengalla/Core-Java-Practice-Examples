89f4a24ed8033e8c416ba072e1f92bcbb6e7e509
Ajay Ullengalla <ullengallaajay28@gmail.com>
2025-11-28 16:08:18 +0530

    Updated

--- src/oops/encapusulation/StudentDetails.java
+++ src/oops/encapusulation/StudentDetails.java
@@ -3,11 +3,12 @@ package oops.encapusulation;
 public class StudentDetails {
 
 	
-	static void show(Student student) {
+	static void show(Student student,Employee emp) {
 		System.out.println(student.getStudentId());
 		System.out.println(student.getStudentName());
 		System.out.println(student.getMobileNumber());
 		System.out.println(student.getEmail());
+		System.out.println(emp.getEmpName());
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -17,13 +18,16 @@ public class StudentDetails {
 		st.setStudentName("Ajay");
 		st.setMobileNumber(994965522);
 		st.setEmail("ajay123@gmail.com");
+		Employee emp = new Employee();
 		
-		show(st);
+		emp.setEmpName("Ajju");
+		show(st,emp);
 		st.setStudentId(2);
 		st.setStudentName("Mani");
 		st.setMobileNumber(994965522);
 		st.setEmail("mani123@gmail.com");
-		show(st);
+		show(st,emp);
+		
 	}
 
 }
