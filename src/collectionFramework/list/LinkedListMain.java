261cefb31163611cd71fbbce8a4ba0234a88edf8
Ajay Ullengalla <ullengallaajay28@gmail.com>
2025-12-08 18:11:15 +0530

    Uploaded LinkedList

--- src/collectionFramework/list/LinkedListMain.java
+++ src/collectionFramework/list/LinkedListMain.java
@@ -1,7 +1,9 @@
 package collectionFramework.list;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 
 public class LinkedListMain {
 
@@ -40,8 +42,38 @@ public class LinkedListMain {
 		linkedList.removeLast();
 		System.out.println("Removed Element at Last: "+linkedList);
 		
+		//sort
+		Collections.sort(linkedList);
+		System.out.println("AfterSorting: "+ linkedList);
 		
+		// ---> Index
+		System.out.println("Highest  : "+Collections.max(linkedList));
+		System.out.println("Peek Element : "+linkedList.peek());
+		System.out.println("Lowest: "+Collections.min(linkedList));
+		
+		//-----> get()
+		
+		System.out.println("value at index 5 "+linkedList.get(5));
+		
+		ArrayList<Integer> numbersList = new ArrayList<>();
+		int[] nums = {5, 12, 27, 33, 48, 59, 72, 90, 40, 12, 5, 27};
 
+		numbersList.add(nums[1]);
+		System.out.println(numbersList);
+		for(int i=0;i<nums.length;i++) {
+			numbersList.add(nums[i]);
+		}
+		
+		linkedList.addAll(numbersList);
+		System.out.println("After Adding ArrayList: "+linkedList);
+
+		Collections.sort(linkedList);
+		System.out.println("AfterSorting: "+ linkedList);
+		
+		List<String> listExample = new ArrayList<>();
+		listExample = new LinkedList();
+		listExample.add("Hello");
+		System.out.println(listExample);
 	}
 
 }
